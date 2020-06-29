c5827711887bc36ec33e4ee89ebda77bc2555c1e
nmparfait <mbamu.developer@gmail.com>
2020-06-29 13:04:47 -0400

    Console app create with classes and program.cs

---
+++ ConsoleApp1/ConsoleApp1/CalculatorEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "substract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized");
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputconverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                Console.WriteLine("Insert the first number");
+                double firstNumber = inputconverter.ConvertInputToNumeric(Console.ReadLine());
+
+                Console.WriteLine("Insert the second number");
+                double secondNumber = inputconverter.ConvertInputToNumeric(Console.ReadLine());
+
+                Console.WriteLine("Insert the operational sign");
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+
+                Console.WriteLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
